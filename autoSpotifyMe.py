ec1087d70be4975526ef854956c1d4b20f516118
J87 <justin.hoffman2013@gmail.com>
2016-10-13 11:21:41 -0500

    Commented Spotify Code

--- autoSpotifyMe.py
+++ autoSpotifyMe.py
@@ -1,4 +1,4 @@
-######################PYSPOTIFY##########################
+######################Stream Using PYSPOTIFY##########################
 
 ###MUST HAVE A SPOTIFY PREMIUM ACCOUNT!!!!
 
@@ -8,14 +8,15 @@
 This is an example of playing music from Spotify using pyspotify.
 The example use the :class:`spotify.AlsaSink`, and will thus only work on
 systems with an ALSA sound subsystem, which means most Linux systems.
-You can either run this file directly without arguments to play a default
-track::
-    python play_track.py
-Or, give the script a Spotify track URI to play::
-    python play_track.py spotify:track:3iFjScPoAC21CT5cbAFZ7b
 
-ref = install -- https://pyspotify.mopidy.com/en/latest/installation/
-ref = https://media.readthedocs.org/pdf/pyspotify/v2.x-develop/pyspotify.pdf
+0. Make sure you have a Spotify Premium account -- it doesnt work for free accounts
+1. Follow these references to create a Spotify Developer account/ make a new app and get the binaries
+	ref = get keys -- https://pyspotify.mopidy.com/en/latest/quickstart/#application-keys
+2. Get PySpotify installed with AlsaSink as well
+	ref = install -- https://pyspotify.mopidy.com/en/latest/installation/
+3. Start developing and if you need to read some documentation
+	ref = https://media.readthedocs.org/pdf/pyspotify/v2.x-develop/pyspotify.pdf
+4. You are only limited by your imagination
 """
 
 from __future__ import unicode_literals
@@ -27,33 +28,41 @@ import spotify
 from spotify import sink
 
 
-
 def on_connection_state_updated(session):
+	"""create a function that listens for the login status"""
     if session.connection.state is spotify.ConnectionState.LOGGED_IN:
         logged_in.set()
 
 
 def on_end_of_track(self):
+	"""listens to see if its the end of the track"""
     end_of_track.set()
 
 
 def getPlaylist(playlistName):
-	"""gets the uris from a playlist"""
-	print "You have ",len(session.playlist_container)," playlists"
+	"""gets the uris from a playlistName you want to search for"""
+	print "You have ",len(session.playlist_container)," playlists on your account"
 	i = 0
+	#print all the playlist names
 	for ea in session.playlist_container:
 		print session.playlist_container[i].load().name
 
+	#you can comment this out, just making sure to logout properly
 	session.logout()
 	session.forget_me()
 
 
 def playTrack(trackURI):
+	"""play any track with any given trackURI"""
 	#####main function to play something
-	# Play a track
+
+	#this is only needed if you dont use Track(u'xxxx')
+	# Play a single track
 	#track_uri = 'spotify:track:'+trackURI
 	#track_uri = trackURI
-	#track = session.get_track(track_uri).load() #this is only needed if you dont use Track(u'xxxx')
+	#track = session.get_track(track_uri).load() 
+
+	#play a track using the info provided by Spotify themselves
 	session.player.load(trackURI)
 	session.player.play()
 
@@ -66,8 +75,7 @@ def playTrack(trackURI):
 	    session.logout()
 	    session.forget_me()
 	    print "logged you out properly...I think"
-	#except KeyboardInterrupt:
-	    #pass
+	
 	except:
 		session.logout()
 		session.forget_me()
@@ -75,7 +83,7 @@ def playTrack(trackURI):
 
 
 def getAlbum(albumURI):
-	"""get the album information"""
+	"""get the album information -- right now only works if you know the album URI"""
 	try:
 		album_uri = 'spotify:album:'+albumURI
 		album = session.get_album(album_uri)
@@ -94,7 +102,7 @@ def getAlbum(albumURI):
 
 
 def getCover(trackURI):
-	"""get the cover and show it"""
+	"""get the cover and show it -- loads the values but doesnt display it for you"""
 	try:
 		cover_uri = 'spotify:album:'+trackURI
 		cover = session.get_album(cover_uri)
@@ -104,16 +112,20 @@ def getCover(trackURI):
 
 
 def getURI(searchTerm):
-	"""input any search term and I'll find it for you"""
+	"""input any search term and I'll find it for you and returns trackNames and trackURIs to use"""
 	try:
 		search = session.search(searchTerm)
 		print search.load()
 		#(search.artist_total, search.album_total, search.track_total, track.playlist_total)
 		print search.artists[0].load().name #get first result
-		print len(search.tracks)
-		print [a.load() for a in search.tracks] #if you leave name off, you get the uris
-		trackNames = [a.load().name for a in search.tracks]
-		trackURIs = [a.load() for a in search.tracks]
+		
+		#eventually will go and get cover art at the same time
+		#getCover(theAlbumURI)
+
+		print len(search.tracks) #the total number of tracks
+		print [a.load() for a in search.tracks] #if you leave "name" off, you get the uris
+		trackNames = [a.load().name for a in search.tracks] #make the list of all the track names
+		trackURIs = [a.load() for a in search.tracks] #make a list with all the track URIs
 		return trackNames,trackURIs
 		#lets play blackbird - which is 1 location
 
@@ -129,7 +141,7 @@ session = spotify.Session()
 loop = spotify.EventLoop(session)
 loop.start()
 
-# Connect an audio sink
+# Connect an audio sink - alsaSink is used on unix systems
 audio = spotify.AlsaSink(session)
 
 
@@ -142,7 +154,7 @@ end_of_track = threading.Event()
 session.on(spotify.SessionEvent.CONNECTION_STATE_UPDATED, on_connection_state_updated)
 session.on(spotify.SessionEvent.END_OF_TRACK, on_end_of_track)
 
-# Assuming a previous login with remember_me=True and a proper logout
+# Assuming a previous login, we may want to logout, especially if remember_me=True previously
 session.logout() #clear out any priors just in case
 session.forget_me()
 
@@ -156,19 +168,26 @@ except:
 #login and wait for the end of the track
 logged_in.wait()
 
-#play the track
 
+##use this to print out what playlists you have
 #getPlaylist(trackURI)
+
+##get the cover info - this one is still in progress
+#getCover(someTrackURIthatYouHave)
 
 #loop through the tracks for the Beatles and Play them all....
 trackNames,trackURIs = getURI("The Beatles")
+#for every trackName and trackURI in the found lists
 for trackName,trackURI in zip(trackNames,trackURIs):
+	#ask if we should play that song
 	answer = str(raw_input("Play "+trackName+" now?(Y,N)>"))
+	#if yes, then stream it
 	if answer != "N":
 		print "Playing ",trackName," ..."
 		playTrack(trackURI)
 	else:
 		pass
 
+	#make sure you log out
 	session.logout()
 	session.forget_me()
